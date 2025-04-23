41c3d05d89842e18e79a51e6bdab743e3c63546a
Larissa Tonetto <larigottschall@hotmail.com>
2025-04-23 16:21:44 -0300

    cenários de login

--- cypress/e2e/exec/login.cy.js
+++ cypress/e2e/exec/login.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Funcionalidade: Login', () => {
+describe('Funcionalidade: Já estou inscrito - Login', () => {
 
     beforeEach (() => {
         cy.visit('https://buzz.dev.inteli.edu.br/signup?src=92d8ec6585cd2d5e3e2dc45723e616eb') 
@@ -10,19 +10,74 @@ describe('Funcionalidade: Login', () => {
   //      cy.screenshot()
   //  }); 
 
-    it('Deve fazer login sem sucesso devido senha ou e-mail incorretos', () => {
+    it('Não deve fazer login com sucesso, pois e-mail está incorreto', () => {
            
         cy.get('.MuiButton-containedPrimary').click()
-        cy.get('#username').type('aluno201@inteli.edu.br')
-        cy.get('#password').type ('X*E4$6')
+        cy.get('#username').type('aluno@inteli.edu.br')
+        cy.get('#password').type ('X*E4$6zv')
         cy.get('.sc-eKtvVk > .MuiButton-root').click()
-
         cy.get('#alert-dialog-description > .MuiTypography-body1').should('contain', 'E-mail e/ou senha incorretos. Verifique seus dados e tente novamente')
         cy.wait(5000)
         cy.get('.MuiButtonBase-root > .MuiTypography-root').click()
       
 
     }),
+
+    it('Não deve fazer login com sucesso, pois senha está incorreta', () => {
+           
+        cy.get('.MuiButton-containedPrimary').click()
+        cy.get('#username').type('aluno201@inteli.edu.br')
+        cy.get('#password').type ('X*E4$6')
+        cy.get('.sc-eKtvVk > .MuiButton-root').click()
+        cy.get('#alert-dialog-description > .MuiTypography-body1').should('contain', 'E-mail e/ou senha incorretos. Verifique seus dados e tente novamente')
+        cy.wait(5000)
+        cy.get('.MuiButtonBase-root > .MuiTypography-root').click()
+      
+
+    }),
+
+    it.only('Não deve fazer login com sucesso, pois e-mail está inválido', () => {
+    
+        cy.get('.MuiButton-containedPrimary').click()
+        cy.get('#username').type('aluno201@inteli')
+        cy.get('#password').type ('X*E4$6zv')
+        cy.get('.sc-eKtvVk > .MuiButton-root').click()
+        cy.get('').should('contain', 'E-mail ou CPF informado é inválido')
+        cy.wait(5000)
+        cy.get('.MuiButtonBase-root > .MuiTypography-root').click()
+    }),
+
+    it.only('Não deve fazer login com sucesso, pois CPF está inválido', () => {
+    
+        cy.get('.MuiButton-containedPrimary').click()
+        cy.get('#username').type('41835429888')
+        cy.get('#password').type ('X*E4$6zv')
+        cy.get('.sc-eKtvVk > .MuiButton-root').click()
+        cy.get('').should('contain', 'E-mail ou CPF informado é inválido')
+        cy.wait(5000)
+        cy.get('.MuiButtonBase-root > .MuiTypography-root').click()
+    }),
+
+    it.only('Não deve fazer login com sucesso, pois campo de e-mail/cpf excede 60 caracteres', () => {
+    
+        cy.get('.MuiButton-containedPrimary').click()
+        cy.get('#username').type('aluno123456789012345678901234567890123456789012@inteli.edu.br')
+        cy.get('#password').type ('X*E4$6zv')
+        cy.get('.sc-eKtvVk > .MuiButton-root').click()
+        cy.get('').should('contain', 'O campo deve ter no máximo 60 caracteres')
+        cy.wait(5000)
+        cy.get('.MuiButtonBase-root > .MuiTypography-root').click()
+    }),
+
+    it.only('Validar ícone olho na senha', () => {
+    
+        cy.get('.MuiButton-containedPrimary').click() 
+        cy.get('#username').type('aluno201@inteli.edu.br')
+        cy.get('#password').type ('X*E4$6zv')
+        cy.get('').click()
+        cy.wait(10000)
+    }),
+
 
     it.only('Deve fazer login com sucesso', () => {
     
@@ -31,14 +86,9 @@ describe('Funcionalidade: Login', () => {
         cy.get('#password').type ('X*E4$6zv')
         cy.get('.sc-eKtvVk > .MuiButton-root').click()
         cy.wait(10000)
-        
-        //cy.get('.MuiBox-root > .MuiTypography-h4').should('be.visible', { timeout: 10000 }).and('contain', 'Vamos começar?');
-        //cy.wait(5000)
-        //cy.get('.MuiBox-root > .MuiTypography-h4').should('contain', 'Vamos começar?')
-
-       // cy.get('.sc-nZgfj > .MuiBox-root > :nth-child(1)').should('be.visible', { timeout: 10000 }).and('have.text', 'Olá')
-    
     })
+
+
 })
 
 
